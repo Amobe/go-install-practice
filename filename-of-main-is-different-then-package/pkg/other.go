6b424cc6bc6c36d901d005a2b5034f370fbe58b2
lexamo <lexamo.liu@gmail.com>
2022-02-17 00:27:42 +0800

    Add failed case: The filename of main is different than package name

---
+++ filename-of-main-is-different-then-package/pkg/other.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("A pkg doesn't contain go.mod and filename is different than the package.")
+}
--- pkg7/other.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("pkg7: without go.mod and with other file name")
-}
